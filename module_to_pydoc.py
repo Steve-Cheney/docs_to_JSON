df475aaadb5d6bd08d04034ffb95d612e503ec94
Steve-Cheney <steve.a.cheney99@gmail.com>
2024-02-23 12:01:07 -0500

    Module types added

--- module_to_pydoc.py
+++ module_to_pydoc.py
@@ -5,7 +5,7 @@ import sys
 def extract_pydoc(functions_file, output_file, module_in, is_silent):
     with open(functions_file, 'r') as f:
         functions = f.readlines()
-
+    good_functions = []
     with open(output_file, 'w') as out_f:
         last_non_empty_line = None
         for function_name in functions:
@@ -23,12 +23,28 @@ def extract_pydoc(functions_file, output_file, module_in, is_silent):
                         module = module_in
                         for submodule_name in submodules:
                             module = getattr(module, submodule_name)
+                            #print(module)
                     
                     function = getattr(module, function_name)
                     pydoc = inspect.getdoc(function)
                     if pydoc:
-                        out_f.write(f"Function: {function_name}\n")
+                        module_name = module_in.__name__.split('.')[0]
+                        if len(submodules)==0:
+                            #print(submodules)
+                            module_group = module_name
+                        else:
+                            submodules_group = '.'.join(submodules)
+                            module_group = f"{module_name}.{submodules_group}"
+                            
+                        full_func_group = f"{module_group}.{function_name}"
+                        #print(full_func_group)
+                        good_functions.append(full_func_group)
+                        
+                        out_f.write(f"Function: {full_func_group}\n")
+                        out_f.write(f"Package: {module_name}\n")
+                        out_f.write(f"Submodules: {'.'.join(submodules)}\n")
                         out_f.write(f"{pydoc}\n\n")
+                                                
                         last_non_empty_line = out_f.tell()  # Remember the position of the last non-empty line
                     elif not is_silent:
                         print(f"No pydoc found for function: {function_name}\n")
@@ -40,6 +56,11 @@ def extract_pydoc(functions_file, output_file, module_in, is_silent):
     if last_non_empty_line is not None:
         with open(output_file, 'a') as out_f:
             out_f.truncate(last_non_empty_line)
+    
+    with open("good_funcs.txt", 'w') as file:
+        for func in good_functions:
+            file.write(f"{func}\n")
+
 
 if __name__ == "__main__":
     if len(sys.argv) <   4:
